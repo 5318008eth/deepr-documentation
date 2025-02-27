66f77599046c8c355d692c9bf715279f67579a1b
King Toadstool <SporeNFTs@proton.me>
2025-02-27 13:12:41 +1000

    update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,31 @@
 import { Metadata } from "next";
 import { importPage } from "nextra/pages";
 import { useMDXComponents } from "@/mdx-components";
-
-const Wrapper = useMDXComponents().wrapper;
+import type { NextraMetadata, Heading } from 'nextra';
 
 export const metadata: Metadata = {
   title: "Deepr",
   description: "Deepr Documentation"
 };
 
+interface ContentWrapperProps {
+  children: React.ReactNode;
+  toc: Heading[];
+  metadata: NextraMetadata;
+}
+
+function ContentWrapper({ children, toc, metadata }: ContentWrapperProps) {
+  const { wrapper: Wrapper } = useMDXComponents();
+  return <Wrapper toc={toc} metadata={metadata}>{children}</Wrapper>;
+}
+
 export default async function Page() {
   const result = await importPage(["onepager"]);
   const { default: MDXContent, toc, metadata } = result;
   
   return (
-    <Wrapper toc={toc} metadata={metadata}>
+    <ContentWrapper toc={toc} metadata={metadata}>
       <MDXContent />
-    </Wrapper>
+    </ContentWrapper>
   );
 }
